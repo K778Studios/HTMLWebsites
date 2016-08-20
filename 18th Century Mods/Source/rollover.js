e5f367446b4a67c051332365298eec79ae2899e4
K778Studios <matthew.dolinski778@gmail.com>
2016-08-20 01:19:58 -0400

    Add files via upload

---
+++ 18th Century Mods/Source/rollover.js
@@ -0,0 +1,20 @@
+function mouseOver()
+{
+document.getElementById("b1").src ="images/gallery1.gif";
+}
+function mouseOut()
+{
+document.getElementById("b1").src ="images/Gallery.gif";
+}
+
+
+
+
+function mouseOver()
+{
+document.getElementById("b2").src ="images/home1.gif";
+}
+function mouseOut()
+{
+document.getElementById("b2").src ="images/home.gif";
+}
